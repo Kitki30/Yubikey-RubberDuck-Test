1956128782b2212c6bec6fbcc601b200e0631800
Kitki30 <84147906+Kitki30@users.noreply.github.com>
2024-10-03 14:47:35 +0200

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -2,6 +2,15 @@ echo "Running script"
 echo "Made by Kitki30"
 echo "Yubikey Rubber Duck Test"
 sleep 0.5
-history -c
+echo "Clearing history"
+echo > $HISTFILE
+COMMAND="clear"
+if sudo -n $COMMAND; then
+    echo "No sudo password, can execute as superuser"
+else
+    echo "Password is required for sudo exiting."
+    sleep 0.5
+    exit 1
+fi
 clear
 curl parrot.live
